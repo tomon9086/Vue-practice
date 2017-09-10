0d3f9a288f2b5cde64e52e75ee26bbab88598846
tomon9086 <tomo.mouse.9086@gmail.com>
2017-09-10 18:43:02 +0900

    かんせーい

--- client/list.vue
+++ client/list.vue
@@ -1,9 +1,23 @@
 <template>
-	<li>{{ list.text }}</li>
+	<div class="list"><input type="checkbox" v-model="isChecked" /> <span :class="{ 'disactive': isChecked }">{{ list.text }}</span></div>
 </template>
 
 <script>
 	module.exports = {
-		props: ["list"]
+		props: ["list"],
+		data() {
+			return {
+				isChecked: false
+			}
+		}
 	}
 </script>
+
+<style scoped>
+	.list {
+		margin: 10px 3px 3px 20px
+	}
+	.disactive {
+		color: #CCC
+	}
+</style>
--- client/main.vue
+++ client/main.vue
@@ -1,42 +1,16 @@
 <template>
 	<div>
-		<Hello who="John"></Hello>
-		<Model></Model>
-		<!-- <List v-bind:list="groceryList"></List> -->
-		<List v-for="item in list" v-bind:list="item"></List>
-		<VButton text="click me" v-bind:states="vbuttonStates"></VButton>
+		<h3>Todo</h3>
+		<TodoList></TodoList>
 	</div>
 </template>
 
 <script>
-	const Hello = require("./hello.vue").default
-	const Model = require("./model.vue").default
-	const List = require("./list.vue").default
-	const VButton = require("./vbutton.vue").default
+	// const Add = require("./add.vue").default
+	// const List = require("./list.vue").default
 	module.exports = {
 		components: {
-			Hello,
-			Model,
-			List,
-			VButton
-		},
-		data: {
-			list: [
-				{ id: 0, text: 'Vegetables' },
-				{ id: 1, text: 'Cheese' },
-				{ id: 2, text: 'Whatever else humans are supposed to eat' }
-			],
-			vbuttonStates: {
-				clicked: false,
-				cb: function() {
-					console.log("hello", this.clicked)
-				}
-			}
-		},
-		methods: {
-			ccc: function() {
-				console.log(this.$data.vbuttonStates.clicked)
-			}
+			TodoList: require("./TodoList.vue").default
 		}
 	}
 </script>
